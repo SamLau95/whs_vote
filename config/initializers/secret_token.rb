56c25988a2eddd095d37221f230a256fa83454c0
magurt14 <samlau95@gmail.com>
2013-06-24 20:21:02 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhsVote::Application.config.secret_token = 'd072b557eb5182bad2b505e5ba473a27a62617594bd4c26f47eef47b2f0f0c026d026440decb1056992f5e7fb0fb012bd41b509a8445c6cf1201613dc35eca4e'
